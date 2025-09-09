95479c5246ef70c6bffeedc1b79859dbed65e9c5
hemavardhini2006 <hemavardhini63338@gmail.com>
2025-09-09 07:17:19 +0530

    Day 13 : Find Missing Number

---
+++ MissNum.java
@@ -0,0 +1,16 @@
+class MissNum {
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {9,6,4,2,3,5,7,0,1};
+        System.out.println(missingNumber(nums)); 
+    }
+}
