8fcfe6c3ce110917e73c43d0135344dcc19829f9
hemavardhini2006 <hemavardhini63338@gmail.com>
2025-12-27 16:23:34 +0530

    Day - 30 :Reverse LL K time

---
+++ ReverseLLKtimes.java
@@ -0,0 +1,51 @@
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class ReverseLLKtimes {
+    public static Node reversal(Node head,int k){
+    if(head == null)
+        return null;
+    Node curr = head;
+    Node pre = null;
+    Node next = null;
+    int cout =0;
+    while(curr != null && cout<k){
+      next = curr.next;
+      curr.next = pre;
+      pre = curr;
+      curr = next;
+      cout++;
+    } 
+    
+    if(next != null){
+        head.next = reversal(next,k);
+    }
+    return pre;
+}
+   public static void sol(Node head){
+    Node temp = head;
+    while(temp != null){
+        System.out.println(temp.data+" "+"-->");
+        temp = temp.next;
+    }
+   }
+    public static void main(String[] args){
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3); 
+        head.next.next.next = new Node(4); 
+        head.next.next.next.next = new Node(5);
+        int k = 2;
+        System.out.println("orignal LL is");
+        sol(head);
+        System.out.println("after K reversal");
+        Node head2 = reversal(head,k);
+        sol(head2);
+    }
+}
