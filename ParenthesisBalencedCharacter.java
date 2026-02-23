929b3c65d505a461b948bdf1ea8e797e696f8d2d
hemavardhini2006 <hemavardhini63338@gmail.com>
2026-02-23 21:24:42 +0530

    Day - 36 : parenthesis balence check

---
+++ ParenthesisBalencedCharacter.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class ParenthesisBalencedCharacter {
+    public static boolean iscorrect(String s){
+      Stack<Character>stack = new Stack<>();
+      for(int i=0;i<s.length();i++){
+        char ch = s.charAt(i);
+        if(ch=='(' || ch=='{' || ch=='['){
+            stack.push(ch);
+        }
+        else{
+            if(stack.isEmpty()){
+                return false;
+            }
+            char top = stack.pop{();
+            if((ch==')' && top!='(' )|| (ch=='}' && top!='{') || (ch==']' && top!='[')){
+                return false;
+            }
+        }
+    }
+        return stack.isEmpty();
+      }
+    public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+     System.out.println("enter the balenced parenthesis");
+     String wordding = sc.nextLine();
+     boolean result = iscorrect(wordding);
+     System.out.println(result);
+    }
+}
